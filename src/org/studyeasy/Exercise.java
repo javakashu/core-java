521bf4bf9725ce7bd00101e7dba418d3bcefb787
akshu <javakashu@gmail.com>
2023-06-04 17:36:19 -0500

    Committed

---
+++ src/org/studyeasy/Exercise.java
@@ -0,0 +1,23 @@
+package org.studyeasy;
+
+public class Exercise {
+    // (a+b)square = ((a*a) + 2*a*b +(b*b))
+
+    public static void main(String[] args) {
+        int a= 25 ;
+        float b= 42.159f;
+
+        int value=(int)(a*a +2*(a*b)+b*b);
+        System.out.println("value of the equation is "+value);
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/org/studyeasy/Hello.java
@@ -0,0 +1,39 @@
+package org.studyeasy;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+//        int y = 65000;
+//        short x =(short)y;
+//        System.out.println(x);
+//
+//
+//        double v2 = 20.0123456789d;
+//        float v1 = (float)v2;
+//        System.out.println(v1);
+
+
+//        float f =10.532f;
+//        long l = (long)f;
+//        System.out.println(l);
+
+//        byte b =127;
+//        int a = 257;
+//        System.out.println(a);
+//
+//        float f =5.6f;
+//        int t = (int)f;
+//        System.out.println(t);
+//        byte k = (byte)a;
+//
+//
+
+
+    }
+
+
+
+
+
+}
